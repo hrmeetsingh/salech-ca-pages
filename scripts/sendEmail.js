5ef2f73208364cd72caeb560083b8f118a30db2c
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-10-12 21:05:50 -0600

    Update sendEmail.js

--- scripts/sendEmail.js
+++ scripts/sendEmail.js
@@ -2,5 +2,5 @@ function sendEmail() {
     const sender = document.getElementById("sender-email");
     const messageBody = encodeURIComponent("Keep me updated");
     const subject = encodeURIComponent("Interested");
-    window.location.href=`mailto:harmeetsalech@gmail.com?subject=${subject}&body=${messageBody}&from=${sender}`;
+    window.location.href=`mailto:harmeet@salech.ca?subject=${subject}&body=${messageBody}&from=${sender}`;
 }
